d7f269d1fea613888f898bae641af7a572e36221
tanbiralam <itzmetanbir@gmail.com>
2024-07-28 11:10:09 +0530

    Select Component minor issue fixed

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -7,20 +7,20 @@ export function Select({
   label,
   isDisabled,
 }: {
-  value: string;
+  value: string | number;
   label: string;
   onChange: (value: ChangeEvent<HTMLSelectElement>) => void;
   options: { value: string | number; name: string }[];
   isDisabled?: boolean;
 }) {
   return (
-    <div className="flex flex-col gap-1 items-start">
-      <label htmlFor={label} className="text-gray-300 text-xs">
+    <div className="flex flex-col items-start gap-1">
+      <label className="text-xs text-gray-300 ml-1" htmlFor={label}>
         {label}
       </label>
       <select
         disabled={isDisabled}
-        className="bg-gray-700 rounded-md cursor-pointer hover:bg-gray-800 transition ease-in active:ring-0 active:border-0 p-2 min-w-[200px] sm:min-w-full"
+        className="bg-gray-700 disabled:pointer-events-none rounded-md cursor-pointer hover:bg-gray-800 transition ease-in active:ring-0 active:border-0 p-2 min-w-[200px] sm:min-w-full"
         id={label}
         value={value}
         onChange={onChange}
